c2706dd8f0ea30aa23bb03fb022ff82d1e9beafd
bacrawu <bacrawu@tencent.com>
2024-04-27 20:13:59 +0800

    Add: export TranslateData type

--- packages/i18nt/src/index.ts
+++ packages/i18nt/src/index.ts
@@ -1,3 +1,5 @@
 // 需要导出 type I18NHandler，方便 initI18N 使用
 export { i18nt, type I18NHandler } from './i18nt';
 export { initI18N } from './initI18N';
+
+export type { TranslateData, I18NFullOptions } from './lib/translate';
