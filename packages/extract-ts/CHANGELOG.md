ad58adfa9c3fedc775621ddf80ca7bd37f3323e5
bacrawu <bacrawu@tencent.com>
2024-05-14 02:20:05 +0800

    Rename test

--- packages/extract-ts/CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-# @i18n.t/extract-ts
-
-## 1.0.1
-
-### Patch Changes
-
-- Use Jest
--- packages/getlanguages/src/index.ts
+++ packages/getlanguages/src/index.ts
@@ -1,5 +1,5 @@
 import * as geLanguages0 from './lib/getLanguages';
-import * as nodeParser0 from './lib/node-parser';
+import * as nodeParser0 from './lib/nodeParser';
 
 export const getLanguages = geLanguages0;
 export const node = nodeParser0;
